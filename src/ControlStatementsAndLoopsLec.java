1174daf7369d204a4567d0014e6a9ea16de59b30
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-09-03 16:23:06 -0500

    Control Statements and Loops lecture

---
+++ src/ControlStatementsAndLoopsLec.java
@@ -0,0 +1,68 @@
+public class ControlStatementsAndLoopsLec {
+    public static void main(String[] args) {
+    // you can solve nay programming problem with just
+    // Statements
+    // Decisions
+    // Loops
+
+    // You can:
+    // Do something
+    // Do something under a certain condition
+    // Do something again and again
+
+    // Key differences about java statements with respect to Javascript statements:
+    // no Automatic Semicolon Insertion
+    // all variables need a type declaration
+
+    // Conditional branching
+//    int x  = 5;
+//    if (x == 5) {
+//        System.out.println("If you are reading this, x must be 5 since this is running");
+//    }
+//    System.out.println("On the other hand, this doesn't mean anything because it runs regardless of what x is");
+
+//    int y = 2;
+//    y = 3;
+//    if (y == 2) {
+//        System.out.println("I've carried out a conditional test and I can confirm that the statement 'y == 2' is true");
+//    } else {
+//        System.out.println("The result of my conditional test was false");
+//    }
+
+//        System.out.println(y == 2);
+
+//    short y = -5;
+//    y = 12;
+//    y = 32450;
+    // y = 33000; //incompatible types: possible lossy conversion from int to short
+
+//    if (y < 0 ) {
+//        System.out.println("The variable y contains a negative number");
+//    } else if (y >=0 && y < 30000){
+//        System.out.println("The variable y contains a positive number");
+//    } else {
+//        System.out.println("The variable y is dangerously close to the max value for short type variables, which is " + Short.MAX_VALUE);
+//    }
+
+    String customerChoice = "strawberry";
+
+    switch (customerChoice) {
+        case "vanilla":
+            System.out.println("One vanilla coming right up");
+            break;
+        case "chocolate":
+            System.out.println("chocolate coming right up");
+            break;
+        case "strawberry":
+            System.out.println("strawberry coming right up");
+            break;
+        default:
+            System.out.println("We don't have that");
+            break;
+    }
+
+
+
+
+    }
+}
