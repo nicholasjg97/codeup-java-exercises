b72d398ba059af05152adf5f78827e20fb228327
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-09-03 14:12:30 -0500

    finished exercises for Console IO

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+        double pi = 3.14159;
+//        System.out.println("The value of pi is approximately " + pi);
+//        System.out.printf("The value of pi is approximately %s", pi);
+//        System.out.format("The value of pi is approximately %.4s" , pi);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter an integer: ");
+//        int nextInt = scanner.nextInt();
+//        System.out.println("The integer you entered is " + nextInt);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter 3 words separately: ");
+//        String firstWord = scanner.next();
+//        String secondWord = scanner.next();
+//        String thirdWord = scanner.next();
+//        System.out.printf("The words you have entered are %s, %s, and %s", firstWord, secondWord, thirdWord);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a sentence: ");
+////        String userSent = scanner.next();
+//        String userSent = scanner.nextLine();
+//        System.out.printf("The sentence you wrote is: %s", userSent);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter the length and width values of a classroom at Codeup: ");
+//        int length = Integer.parseInt(scanner.nextLine());
+//        int width = Integer.parseInt(scanner.nextLine());
+//        System.out.println("The area of the classroom you described is " + (length * width) + ". The perimeter equals to " + ((width * 2) + (length * 2)) + ".");
+
+
+
+    }
+
+}
