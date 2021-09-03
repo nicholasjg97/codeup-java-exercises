3f7c899ccceedadf1c125211cbc14a6d767e535d
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-09-03 10:48:25 -0500

    complete syntax-types-variables exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,51 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//        System.out.print("Learning a whole new language already! :(");
+//        System.out.println("It's already confusing!");
+
+//        int myFavoriteNumber = 50;
+//        System.out.println(myFavoriteNumber);
+
+//        String myString = "Does this work?";
+//        System.out.println(myString);
+
+//        long myNumber = 123;
+
+//        float myNumber = 3.14F;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class = "Does this work?";
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y += x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+        System.out.println(Integer.MAX_VALUE);
+
+        int testInteger = 2147483647;
+        System.out.println(++testInteger);
+
+    }
+
+}
