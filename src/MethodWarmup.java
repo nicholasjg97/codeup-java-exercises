059532ff3dfd0ace36a61939c347b544e6970659
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-09-09 11:54:53 -0500

    added recursion method exercise

--- src/MethodWarmup.java
+++ src/MethodWarmup.java
@@ -21,4 +21,15 @@ public class MethodWarmup {
         return a - b;
     }
 
+
+    // TODO: use recursion to add all numbers up, from 1 to the given number input
+    /*
+        addNums(5) would trigger a calculation of...
+            5 + 4 + 3 + 2 + 1, evaluating to 15
+     */
+     public static int addNum(int num) {
+        if (num == 1) return 1;
+        return addNum(num - 1) + num;
+     }
+
 }
