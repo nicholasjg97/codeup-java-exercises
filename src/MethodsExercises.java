ade96659fba9e878a9cdc7407624617e560c8247
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-09-09 16:44:28 -0500

    finished factorial

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -35,13 +35,35 @@ public class MethodsExercises {
         return getInteger(min, max);
     }
 
+    public static void factorial() {
+        Scanner sc = new Scanner(System.in);
+        String continueInput = "";
+//
+        do {
+            System.out.println("Enter a number between 1 and 10:");
+            int userInt = sc.nextInt();
+            long fact = 1;
+            if (userInt < 1 || userInt > 10) {
+                System.out.println("That number is not between 1 and 10");
+            } else {
+                for (int i = 1; i <= userInt; i++) {
+                    fact *= i;
+                }
+                System.out.println("The factorial is " + fact);
+            }
+            System.out.println("Do you like to continue? Yes/No");
+            continueInput = sc.next();
+        } while (continueInput.equals("Yes"));
+    }
+
     public static void main(String[] args) {
-        System.out.println(Addition(3, 6));
-        System.out.println(Subtraction(6, 3));
-        System.out.println(Multiplication(6, 3));
-        System.out.println(Division(6, 3));
-        System.out.println(Modulus(9, 2));
-        getInteger(1, 10);
+//        System.out.println(Addition(3, 6));
+//        System.out.println(Subtraction(6, 3));
+//        System.out.println(Multiplication(6, 3));
+//        System.out.println(Division(6, 3));
+//        System.out.println(Modulus(9, 2));
+//        getInteger(1, 10);
+        factorial();
     }
 
 }
