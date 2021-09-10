f3f91d384ae345f861afdbf4b4b9d831ea3f6948
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-09-10 08:34:38 -0500

    finished all the way to fizzbuzz

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,73 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+//        Below will be an infinite loop unless set to (i > 0) in the while. why?
+//        Will infinite loop if while is set to any number higher than 65536
+
+//        int i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i > 0 && i < 1000000);
+
+//        unless 'data-type' is changed to 'long'
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
+
+//        for loops
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.println(i + " ");
+//        }
+
+//        for (int i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 100; i >= -10; i -=5) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 2; i > 0 && i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+
+//        'data-type' changed to long
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+
+//        FizzBuzz
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("Fizzbuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+
+
+
+
+    }
+
+}
