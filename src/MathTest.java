3a3f7c1975dd9d8a5034228f7a99384847f4ed6c
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-09-13 09:56:06 -0500

    java warmup

---
+++ src/MathTest.java
@@ -0,0 +1,10 @@
+public class MathTest {
+
+    public static void main(String[] args) {
+//        MyMathMethods mathMethods = new MyMathMethods();
+//        System.out.println(mathMethods.multiply(4, 5));
+        System.out.println(MyMathMethods.multiply(5, 4));
+    }
+
+
+}
---
+++ src/MyMathMethods.java
@@ -0,0 +1,10 @@
+public class MyMathMethods {
+
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+
+}
