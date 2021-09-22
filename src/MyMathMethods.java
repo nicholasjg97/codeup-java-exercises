8f94def3f4ede0f058b134ef0128c8e077f68d12
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-09-22 10:35:27 -0500

    mini exercises review overloading

--- src/MyMathMethods.java
+++ src/MyMathMethods.java
@@ -5,6 +5,35 @@ public class MyMathMethods {
         return a * b;
     }
 
+    // 1. use method overloading to create a public static multiply method tht works with doubles
+
+    public static double multiply(double num1, double num2) {return num1 * num2;}
+
+    // 2. write a public static method named quotient that works with both integers and doubles (use method overloading) and returns the result of dividing the two passed numbers.
+
+    public static int quotient(int num1, int num2) {return num1 / num2;}
+
+    public static double quotient(double num1, double num2) {return num1 / num2;}
+
+    // 3. write a public static method named sum that takes in an array of int numbers (not a list, an array), and returns the sum of the numbers
+
+    public static int arraySum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    // 4. Create a class called Movie. The class should define two instance variables of type String named name and director. Add a constructor that takes two strings and sets the name and director properties based on the passed arguments. Make it so the instance variables are not accessible from outside the Movie class. Write getters and setters for the instance variables.
+
+
+
+
+    // Bonus: if the passed name or director arguments are null or empty, the constructor should throw an IllegalArgumentException
+
+
+
 
 
 }
