0c784ac2febdfe8af3d276f92d06ec4cd236c65e
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-10-14 09:36:19 -0500

    warmup exercise

---
+++ src/JavaDrills.java
@@ -0,0 +1,11 @@
+public class JavaDrills {
+
+
+// Create a public static method, `flipOuterCase`, that takes in a string and returns the string with the case reversed for the first and last letters. Assume the input will be three or more characters long.
+    public static String flipOuterCase(String string) {
+        if (string.length() > 3) {
+            return
+        }
+    }
+
+}
